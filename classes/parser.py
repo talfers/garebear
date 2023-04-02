777516969ae3353425a9104b21f4ef60aea8d5b5
Alfers, Taylor <Taylor_Alfers@comcast.com>
2023-04-02 17:48:22 -0600

    clean up

--- classes/parser.py
+++ classes/parser.py
@@ -1,6 +1,5 @@
 from log import logging
 from datetime import datetime
-from bs4 import BeautifulSoup
 from classes.check import Check
 
 logger = logging.getLogger('parser.py')
@@ -8,37 +7,11 @@ logger = logging.getLogger('parser.py')
 class Parser:
     def __init__(self):
         pass
-
-    def make_soup(self, page_source):
-        soup = BeautifulSoup(page_source, features="html.parser")
-        return soup
-
+    
 
     def parse_calendar_data(self, rows):
         print(rows)
-        sites = []
-        # for site in rows:
-        #     site_dates = site.find_all("div", {"class": "rec-grid-grid-cell"})
-        #     site_dict = { "name": "", "dates": [] }
-        #     for i in range(len(site_dates)):
-        #         content = site_dates[i].find("span", {"class": "sarsa-button-content"})
-        #         content = content.contents[0]
-        #         button = site_dates[i].find("button")
-        #         if i == 0:
-        #             site_dict["name"] = content
-        #             continue
-        #         else:
-        #             try:
-        #                 label = button['aria-label']
-        #                 date = label[label.find(' on ') + 4 : label.find("-") - 1]
-        #                 date = datetime.strptime(date, '%B %d, %Y').strftime('%m-%d-%Y')
-        #                 if int(content) > 0:
-        #                     site_dict["dates"].append({"date": date, "value": int(content)})
-        #             except Exception as e:
-        #                 logger.error(f'Error parsing dates! Error: {e}')
-        #     if site_dict['name'] != "" and len(site_dict['dates']) > 0:
-        #         sites.append(site_dict)
-        return sites
+        return rows
 
 
     def convert_dates(self, date_list, format):
